8607369e61f87f6f21b26d7339be3b54fe29a425
CheeGuang <123812015+CheeGuang@users.noreply.github.com>
2024-10-17 22:21:51 +0800

    renamed files to adopt naming convention

--- public/js/navbarFooter.js
+++ public/js/navbarFooter.js
@@ -22,31 +22,6 @@ fetch(navbarFile)
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("navbar-container").innerHTML = data;
-
-    // Check for profile picture and update src if it exists
-    if (pageName.includes("patient")) {
-      const patientDetails = JSON.parse(localStorage.getItem("patientDetails"));
-      console.log("Patient Details:", patientDetails);
-      if (patientDetails && patientDetails.profilePicture) {
-        document.getElementById("profile-image-login").src =
-          patientDetails.profilePicture;
-        console.log(
-          "Patient Profile Picture Set:",
-          patientDetails.profilePicture
-        );
-      }
-    } else if (pageName.includes("doctor")) {
-      const doctorDetails = JSON.parse(localStorage.getItem("doctorDetails"));
-      console.log("Doctor Details:", doctorDetails);
-      if (doctorDetails && doctorDetails.profilePicture) {
-        document.getElementById("profile-image-login").src =
-          doctorDetails.profilePicture;
-        console.log(
-          "Doctor Profile Picture Set:",
-          doctorDetails.profilePicture
-        );
-      }
-    }
   })
   .catch((error) => console.error("Error loading navbar:", error));
 
@@ -57,7 +32,3 @@ fetch("footer.html")
     document.getElementById("footer").innerHTML = data;
   })
   .catch((error) => console.error("Error loading footer:", error));
-
-function openAPIDoc() {
-  window.location.href = window.location.origin + "/api-docs";
-}
