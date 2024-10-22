e582cf2134e6e6c22a819b599da454955cde890c
HRx88 <s10262513@connect.edu.sg>
2024-10-22 10:25:10 +0800

    admin home & admin ManageEvents are done

---
+++ public/js/adminEditManageEvents.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(Loadingpage, 3000);
+ 
+});
+
+function Loadingpage() {
+  // Hide the loading page
+  document.getElementById("loading-page").style.display = "none";
+}
