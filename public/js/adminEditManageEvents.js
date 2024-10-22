fd488261112941c4c8ea9661f7006f73b6406477
HRx88 <s10262513@connect.edu.sg>
2024-10-22 10:27:46 +0800

    delete adminEditManageEvents

--- public/js/adminEditManageEvents.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(Loadingpage, 3000);
- 
-});
-
-function Loadingpage() {
-  // Hide the loading page
-  document.getElementById("loading-page").style.display = "none";
-}
