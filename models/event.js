ff12c1c187efb1cf5a5b18a0ccf455549102a15f
CheeGuang <123812015+CheeGuang@users.noreply.github.com>
2024-11-07 00:03:21 +0800

    modified Telegram function to send link to workshop

--- models/event.js
+++ models/event.js
@@ -240,7 +240,9 @@ class event {
   Don't miss out on this fantastic opportunity! 
   Join us for an enriching experience. 
   If you have any questions, feel free to ask!
-  Sign up now at https://mindsphere.onrender.com/`;
+  Sign up now at https://mindsphere.onrender.com/guestWorkshopInformation.html?${
+    eventData.eventID
+  }`;
 
     console.log("Debug: caption prepared =", caption);
 
